736029128d2ecc6932a3ef2002f4b0d206330558
Stefano Martiniani <sm7683@nyu.edu>
2020-01-27 20:06:08 -0600

    moved distances inside lj .m files

--- r1_example1/lj.m
+++ r1_example1/lj.m
@@ -6,3 +6,13 @@ function [energy] = lj(a, b)
     r = distance(a, b);
     energy = 1/r^6 + 1/r^12;
 end
+
+function [r] = distance(a, b)
+    % DISTANCE computes distance between vectors a and b
+    % Input: row vectors a, b
+    % Output: scalar r
+    assert(isequal(size(a), [1 3]) & isequal(size(b), [1 3]))
+    d = b - a; 
+    r2 = dot(d, d); %can also write this as d.*d
+    r = sqrt(r2);
+end
--- r1_example1/lj_fast.m
+++ r1_example1/lj_fast.m
@@ -10,3 +10,13 @@ function [energy] = lj_fast(a, b)
     energy = ir6 + ir12;
 end
 
+function [ir2] = idistance2(a, b)
+    % IDISTANCE2 computes the inverse square distance (1/r^2) between
+    % vectors a and b
+    % Input: row vectors a, b
+    % Output: scalar ir2
+    assert(isequal(size(a), [1 3]) & isequal(size(b), [1 3]))
+    d = b - a; 
+    r2 = dot(d, d); %can also write this as d.*d
+    ir2 = 1/r2;
+end
